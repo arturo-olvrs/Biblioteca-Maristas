f273c36e1d62d91fc5c28877cea9fa9ed42d4444
Arturo Olivares Martos <2004.olivares@gmail.com>
2023-02-26 23:08:36 +0100

    Código PHP

---
+++ PHP/add_success.php
@@ -0,0 +1,42 @@
+<?php
+  //Variables para DataBase
+  $username="uizmimvv2wcd3";
+  $password="9xhqongskab4";
+  $host="danipartal.net";
+  $database="dbq5x9hjq8yxq3";
+
+  //Configuración de la BD
+  $db = new mysqli($host, $username, $password, $database);
+  if ($db->connect_errno != null) {
+    echo "Error número $db->connect_errno conectando a la base de datos.<br>Mensaje: $db->connect_error.";
+    exit(); 
+  }
+  //Configurar el juego de caracteres
+  $db->set_charset('utf8');
+
+  $sql = 'SELECT * FROM Usuarios ORDER BY IdUsuario DESC LIMIT 1;';
+  $resultado = $db->query($sql);
+  $ultimo_usuario = $resultado->fetch_array(MYSQLI_ASSOC);
+?>
+
+
+
+<html lang="es">
+  <head><title>Usuario Registrado</title></head>
+  <body>
+    <h1>Usuario registrado</h1>
+    <p>Se ha registrado un usuario con los siguientes datos:</p> 
+    <? //Obtener de MySql?>
+      <ul>
+        <li>ID: <?= $ultimo_usuario["IdUsuario"]?> </li>
+        <li>Código de Llave: <?= $ultimo_usuario["CodLlave"]?> </li>
+        <li>Nombre: <?= $ultimo_usuario["Nombre"]?> </li>
+        <li>Primer apellido: <?= $ultimo_usuario["PrimerApellido"]?> </li>
+        <li>Segundo apellido: <?= $ultimo_usuario["SegundoApellido"]?> </li>
+        <li>Correo Electrónico: <?= $ultimo_usuario["CorreoElectronico"]?> </li>
+      </ul>
+
+    <br>
+    <p>En caso de error, notificar al encargado.</p>
+  </body>
+</html>
